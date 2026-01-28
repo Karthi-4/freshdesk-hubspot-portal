dc5a0c2e19f112116b285b5cd63131ef49380247
Karthi <karthirvmhss@gmail.com>
2026-01-28 10:45:31 +0530

    chore: prepare codebase for production deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
- await mongoose.connect(process.env.MONGO_URI);
+ await mongoose.connect(process.env.MONGODB_URI, {
+  serverSelectionTimeoutMS: 30000,
+ });
+
  console.log("MongoDB connected");
 };
