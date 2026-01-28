f61de993e1ef04ff10e41e1a74b2fc2a1e4233ff
Karthi <karthirvmhss@gmail.com>
2026-01-28 12:57:50 +0530

    Configure frontend API base URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- import.meta.env.VITE_API_BASE_URL,
+ baseURL: "https://freshdesk-hubspot-portal.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
